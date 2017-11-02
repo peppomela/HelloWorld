0a427aac7c8fae1b76475b62460bd60c440a117a
peppomela <peppomela.26@gmail.com>
2017-11-02 19:54:16 +0100

    Programma che mostra il doppio di un numero MODIFICATO

--- doppio.py
+++ doppio.py
@@ -1,5 +1,4 @@
 #programma che restituisce il doppio del numero inserito
-print "Inserisci un numero"
-valore=input()
+valore=input("Inserisci un numero = ")
 doppio=valore*2
 print "Il valore doppio e' = ",doppio
